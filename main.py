bb4fc22825f68276a79bd649fdd5ecd9c77f8eca
omkarkarale <82709945+omkarkarale@users.noreply.github.com>
2023-03-28 14:18:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import time,os,shutil
+
+path = "path_of_directory" 
+days = 7 
+
+seconds = time.time() - (days * 24 * 60 * 60)
+
+if os.path.exists(path):
+    for root, dirs, files in os.walk(path):
+        for name in files + dirs:
+            full_path = os.path.join(root, name)
+            ctime = os.stat(full_path).st_ctime
+            if ctime < seconds:
+                if os.path.isfile(full_path):
+                    os.remove(full_path)
+                else:
+                    shutil.rmtree(full_path)
+else:
+    print("Path not found")
